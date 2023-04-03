8cfb87a03ed08efe547860100606a61031bd1f43
Rahugg <rahugg07@gmail.com>
2023-04-03 16:34:08 +0600

    changed icons

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,7 +4,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NavbarComponent} from "./navbar/navbar.component";
 import {ProfileComponent} from "./profile/profile.component";
 import {MatListModule} from "@angular/material/list";
 import {MatIconModule} from "@angular/material/icon";
@@ -13,7 +12,7 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
-    AppComponent, NavbarComponent, ProfileComponent
+    AppComponent, ProfileComponent
   ],
   imports: [
     BrowserModule,
